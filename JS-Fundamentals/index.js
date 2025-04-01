d12623f728ceb758718b58bedc900beb3d4234a7
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-04-01 11:04:51 +0530

    DataType and Variable

--- JS-Fundamentals/index.js
+++ JS-Fundamentals/index.js
@@ -1,1 +1,15 @@
-console.log("Hello Sanjay");
+var fName = "Sanjay";
+var lName = "Vasuniya";
+var age = 20;
+var thirdName= null;
+var jobRole;// undefined
+var Education = "Btech";
+var cNAME = "Sage University";
+
+  
+console.log(fName,lName);
+
+console.log("Hello everyone this is ",fName,lName ,"and I am Puruing ",Education, "from ",cNAME);
+
+console.log("My name is ",fName,lName, "and my age is ", age);
+
