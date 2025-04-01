f504c90484098ddb9c6cf93c6556e5d5260585a3
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-04-01 11:12:56 +0530

    Data type and variable

--- JS-Fundamentals/index.js
+++ JS-Fundamentals/index.js
@@ -1,3 +1,13 @@
+ // DATA TYPE///////////////////////////
+ //1. String = "sanjay vasuniya", "243", "true"
+ //2. boolean = true , false
+ //3. number = 22,2.3,4434,
+ //4. symbol
+ //5. BigInt = 0987654322345678974103698452398523984523985
+ //6. undefined
+ //7. null =
+
+// identifires and leteral
 var fName = "Sanjay";
 var lName = "Vasuniya";
 var age = 20;
