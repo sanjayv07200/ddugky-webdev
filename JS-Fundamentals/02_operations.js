144f0e4b39b40bf00de6f7389053a4ec4d5eac90
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-04-05 10:10:08 +0530

    learned operator

--- JS-Fundamentals/02_operations.js
+++ JS-Fundamentals/02_operations.js
@@ -1,4 +1,4 @@
-//ARITHMATIC OPERATOR 
+// //ARITHMATIC OPERATOR 
 // var num1 = 10;
 // var num2 = 2;
 // var res1 = num1 + num2; // 12
@@ -17,14 +17,73 @@
 // a--;// a = a - 1; = 11
 // console.log(a)
 
-//COMPARISION OPERATOR 
-var x = 23; 
-var y = 27;
+// //COMPARISION OPERATOR 
+// var x = 23; 
+// var y = 27;
 
-console.log(x<y); // true
-console.log(x>y); // false
-console.log(x>=y);// false
-console.log(x<=y);// true
-console.log(x==y);// false
-console.log(x!=y);// true
+// console.log(x<y); // true
+// console.log(x>y); // false
+// console.log(x>=y);// false
+// console.log(x<=y);// true
+// console.log(x==y);// false
+// console.log(x!=y);// true
 
+
+// USE OF ASSIGNMET OPERATOR
+// var A = 5;
+// console.log(A)
+ // assignment operator
+// A += 2; // after use of addition operator // 
+// console.log(A)
+// ;//7
+// A -= 1;
+// console.log(A)
+// //6
+// A *= 5;
+// console.log(A)
+// //30
+// A /= 3;
+// console.log(A)
+// //10
+// A %= 5; 
+// console.log(A)
+// A **= 2;
+// console.log(A);
+
+ //LOGICAL OPERATOR 
+//AND (&&) OPERATOR
+// var age = 12;
+// if (age >= 18  && age <= 35){
+//     console.log(" your welcome in ddugky family.")
+// } else {
+//     console.log("sorry! you can't join ddugky family.")
+// }
+
+// //USE OF OR (||) OPERATOR
+// var adhar = "YES";
+// var sID = "";
+// if (adhar == "YES"  ||  sID == "YES"){
+//     console.log("student can enter in the school.");
+// }else{
+//     console.log("don't allow");
+// }
+
+// USE OF NOT(!) OPERATOR
+var A = 12;
+var B = 23;
+
+console.log((A<B)); // outcome false
+console.log((A>B)); // outcome true
+
+// comparison operator
+var A = 12;
+var B = 23;
+
+console.log(A == B); // false
+console.log(A > B);  // false
+console.log(A < B);  // true
+console.log(A >= B); // false
+console.log(A <= B); // true 
+console.log(A === B);// false
+console.log(A != B); // true
+console.log(A !== B);// true
