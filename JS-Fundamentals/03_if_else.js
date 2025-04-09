b5779086ea27576bb16b2ba6f86fe58e194bffbf
Sanjay Vasuniya <sanjayv072004@gmail.com>
2025-04-09 12:44:58 +0530

    learned if else

---
+++ JS-Fundamentals/03_if_else.js
@@ -0,0 +1,67 @@
+ /*var number = 90;
+if (number>=33){
+    if (number>=80){
+        console.log("you got distinct.")
+    }
+    else{
+        console.log("you passed.")
+    }
+}
+else{
+    console.log(" you failed. need to study more.")
+}
+*/
+
+/*var number = 23;
+ if(number == 0){
+    console.log("number is zero.");
+}else if(number >= 0){
+    console.log(" number is a positive number. ");
+ }
+ else{
+    console.log("number is a negative number.")
+ }*/
+/*
+var marks =5;
+var Grade;
+var conditionO = marks >=90;
+var conditionG = marks >=80 && marks < 90;
+var conditionA = marks >=70 && marks < 80;
+var conditionB = marks >=50 && marks < 70
+var conditionF = marks <=30
+
+if (conditionO)(console.log("your grade is :",Grade="O"));
+else if(conditionG)(console.log("your grade is : ",Grade="G"));
+else if(conditionA)(console.log("your grade is : ",Grade="A"));
+else if(conditionB)(console.log("your grade is : ",Grade="B"));
+else if(conditionF)(console.log("your grade is : ",Grade ="f"));
+*/
+/*
+var num = 53;
+if(num/2==0){
+    console.log( num,":its an even number")
+}else{
+    console.log(num,"its an odd number")
+}*/
+
+// var  number1 = 24;
+// var number2 = 100;
+// var number3 = 768;
+
+// if(number1>number2){
+//     if(number1>number3){
+//         console.log("number1 is the largest number:",number1);
+//     }else{
+//         console.log("number3 is the largest number:",number3)
+//     }
+// }else if (number2 > number3) {
+//     console.log("number2 is the larest number:",number2)
+// }else{
+//     console.log("number3 is the largest number:",number3)
+// }
+
+
+// for checking that the year is leap year or not
+// var year = 2043;
+// if (year/4==0)console.log("this is year is leap year :",year);
+// else(console.log("this is not a leap year :",year));
---
+++ JS-Fundamentals/04_while_loop.js
@@ -0,0 +1,24 @@
+// print hello world for 10 times 
+
+let num = 1;
+while(num<=10){
+    console.log(num,"Hello World");
+    num++;
+}
+console.log("===============")
+
+ //print number of table 2
+ num = 1;
+while(num<=10){
+    console.log(num,"*",2,"=",num*2);
+    num++;
+}
+
+console.log("--------------------")
+
+// print square of 1 to 10 digit
+num = 1;
+while(num<=10){
+    console.log(num,"*",num,"=",num*num);
+    num++;
+}
